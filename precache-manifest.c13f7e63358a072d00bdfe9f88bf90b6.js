dbc0166a537015a5a0bc8c8b18fddc8dd414b9ab
Albert <albertmik1915@gmail.com>
2020-04-20 19:29:20 +0300

    Updates

---
+++ precache-manifest.c13f7e63358a072d00bdfe9f88bf90b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e22d41f075070bbf9a051f891ca7ec",
+    "url": "/mika-yan/genocide1915/index.html"
+  },
+  {
+    "revision": "30da58c8c302f67caafb",
+    "url": "/mika-yan/genocide1915/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "3618de082541c39db1ab",
+    "url": "/mika-yan/genocide1915/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "30da58c8c302f67caafb",
+    "url": "/mika-yan/genocide1915/static/js/2.41ddd236.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/mika-yan/genocide1915/static/js/2.41ddd236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3618de082541c39db1ab",
+    "url": "/mika-yan/genocide1915/static/js/main.4e2f226d.chunk.js"
+  },
+  {
+    "revision": "2f86c5fd30491fb46dc0",
+    "url": "/mika-yan/genocide1915/static/js/runtime-main.05b66f65.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/mika-yan/genocide1915/static/media/persik.4e1ec840.png"
+  }
+]);
